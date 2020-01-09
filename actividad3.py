885f00d827d6d01a0d3f963788ab7c9b7eacbead
Yorfrank Bastidas <ykbastidasc@gmail.com>
2020-01-08 23:25:18 -0400

    cache to reduce socket calls

--- actividad3.py
+++ actividad3.py
@@ -1,13 +1,21 @@
 from socket import socket
 
-file_numeros = open('./numeros.txt','r')
+file_numeros = open('./numeros.txt', 'r')
 total = 0
 s = socket()
 s.connect(('localhost', 9999))
+arregloEnviados = []
+arregloRecibidos =[]
 for line in file_numeros.readlines():
-	s.sendall(line.encode('utf-8'))
-	reply = s.recv(5)
-	total = total + int(reply)
+	if line in arregloEnviados:
+		index = arregloEnviados.index(line)
+		total = total + arregloRecibidos[index]
+	else:
+		s.sendall(line.encode('utf-8'))
+		reply = s.recv(8)
+		total = total + int(reply)
+		arregloEnviados.append(line)
+		arregloRecibidos.append(int(reply))
 s.close()
 print("ACTIVIDAD 3")
 print(total)
